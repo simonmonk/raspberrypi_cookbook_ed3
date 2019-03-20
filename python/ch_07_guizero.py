84e6cb78d4f439c0399a50c61c2fd2dc7d6e2530
simonmonk <srmonk@gmail.com>
2019-03-20 10:47:22 +0000

    guizero

--- python/ch_07_guizero.py
+++ python/ch_07_guizero.py
@@ -1,3 +1,9 @@
-from guizero import App
-app = App(title="Hello world")
+from guizero import *
+
+def say_hello():
+    info("An Alert", "Please don't press this button again")
+
+app = App(title="Pi Cookbook Example", height=200)
+button = PushButton(app, text="Don't Press Me", command=say_hello)
+
 app.display()
