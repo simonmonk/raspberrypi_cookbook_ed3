9bb8b7b619eab70d92509aeb7820914ca85555b9
Simon Monk <srmonk@gmail.com>
2019-05-21 17:37:40 +0100

    servo adafruit

--- python/ch_11_motor_control.py
+++ python/ch_11_motor_control.py
@@ -1,33 +1,13 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Motor
 
-enable_pin = 18
-in1_pin = 23
-in2_pin =24
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(in1_pin, GPIO.OUT)
-GPIO.setup(in2_pin, GPIO.OUT)
-
-pwm = GPIO.PWM(enable_pin, 500)
-pwm.start(0)
-
-def clockwise():
-    GPIO.output(in1_pin, True)    
-    GPIO.output(in2_pin, False)
- 
-def counter_clockwise():
-    GPIO.output(in1_pin, False)
-    GPIO.output(in2_pin, True)
+motor = Motor(forward=23, backward=24)
  
 while True:
-    cmd = raw_input("Command, f/r 0..9, E.g. f5 :")
+    cmd = input("Command, f/r 0..9, E.g. f5 :")
     direction = cmd[0]
+    speed = float(cmd[1]) / 10.0
     if direction == "f":
-        clockwise()
+        motor.forward(speed=speed)
     else: 
-        counter_clockwise()
-    speed = int(cmd[1]) * 10
-    pwm.ChangeDutyCycle(speed)
+        motor.backward(speed=speed)
 
