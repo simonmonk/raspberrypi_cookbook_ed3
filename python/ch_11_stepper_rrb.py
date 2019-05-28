499087f2df01026475e3ea6dfa4a61cfcc5c6a39
Simon Monk <srmonk@gmail.com>
2019-05-28 12:29:19 +0100

    stepper and rrb4

--- python/ch_11_stepper_rrb.py
+++ python/ch_11_stepper_rrb.py
@@ -1,14 +1,14 @@
-from rrb3 import *
+from rrb4 import *
 import time
 
-rr = RRB3(12.0, 12.0) # battery, motor
+rr = RRB4(12.0, 12.0) # battery, motor
 
 try: 
     while True:
-        delay = raw_input("Delay between steps (milliseconds)?")
-        steps = raw_input("How many steps forward? ")
+        delay = input("Delay between steps (milliseconds)?")
+        steps = input("How many steps forward? ")
         rr.step_forward(int(delay) / 1000.0, int(steps))
-        steps = raw_input("How many steps backwards? ")
+        steps = input("How many steps backwards? ")
         rr.step_reverse(int(delay) / 1000.0, int(steps))
 
 finally:
