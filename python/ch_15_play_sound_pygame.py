a893522306717e9e81bd194ab568480ade34df05
Simon Monk <srmonk@gmail.com>
2019-06-26 09:36:53 +0100

    sound

---
+++ python/ch_15_play_sound_pygame.py
@@ -0,0 +1,10 @@
+import pygame
+
+sound_file = '/home/pi/raspberrypi_cookbook_ed3/python/school_bell.mp3'
+
+pygame.mixer.init()
+pygame.mixer.music.load(sound_file)
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy() == True:
+    continue
