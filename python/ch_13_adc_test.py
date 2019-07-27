13d675759ec735dd500ef02657b1695893c4df63
simonmonk <evilgeniusauthor@gmail.com>
2019-07-27 14:43:57 +0100

    ch7

--- python/ch_13_adc_test.py
+++ python/ch_13_adc_test.py
@@ -6,5 +6,5 @@ analog_input = MCP3008(channel=0)
 while True:
     reading = analog_input.value
     voltage = reading * 3.3 
-    print("Reading=%f\tVoltage=%f" % (reading, voltage))
+    print("Reading={:.2f}\tVoltage={:.2f}".format(reading, voltage))
     time.sleep(1)
--- python/ch_13_adc_tmp36.py
+++ python/ch_13_adc_tmp36.py
@@ -8,5 +8,5 @@ while True:
     voltage = reading * 3.3 
     temp_c = voltage * 100 - 50
     temp_f = temp_c * 9.0 / 5.0 + 32
-    print("Temp C=%f\t\tTemp f=%f" % (temp_c, temp_f))
+    print("Temp C={:.2f}\tTemp F={:.2f}".format(temp_c, temp_f))
     time.sleep(1)
