bb6c229db359b8fbe3dc31c677859bde5785e9a2
simonmonk <srmonk@gmail.com>
2020-02-12 12:53:17 +0000

    added movement detection

--- python/ch_08_detect_motion.py
+++ python/ch_08_detect_motion.py
@@ -1,16 +1,25 @@
-from SimpleCV import *
+import cv2
+import numpy as np
+from imutils.video import VideoStream
+from imutils import resize
 
-MIN_BLOG_SIZE = 1000
+diff_threshold = 1000000
 
-c = Camera()
+vs = VideoStream(src=0).start()
 
-old_image = c.getImage()
+def getImage():
+    im = vs.read()
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im = cv2.blur(im, (20, 20))
+    return im
+
+old_image = getImage()
 
 while True:
-    new_image = c.getImage()
-    diff = new_image - old_image
-    blobs = diff.findBlobs(minsize=MIN_BLOG_SIZE)
-    if blobs :
+    new_image = getImage()
+    diff = cv2.absdiff(old_image, new_image)
+    diff_score = np.sum(diff)
+    # print(diff_score)
+    if diff_score > diff_threshold:
         print("Movement detected")
     old_image = new_image
-    print('.')
