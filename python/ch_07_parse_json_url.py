34da8ee7d4e1f5743eded358c9f4864f774d5599
simonmonk <srmonk@gmail.com>
2020-02-12 14:00:48 +0000

    fixed apxu example

--- python/ch_07_parse_json_url.py
+++ python/ch_07_parse_json_url.py
@@ -3,7 +3,7 @@ import urllib.request
 
 key = 'paste_your_key_here'
 
-response = urllib.request.urlopen('http://api.apixu.com/v1/current.json?key=' + key + '&q=Paris')
+response = urllib.request.urlopen('http://api.weatherstack.com/current?access_key=' + key + '&query=Paris')
 j = json.load(response)
 
-print(j['current']['condition']['text'])
+print(j['current']['weather_descriptions'][0])
